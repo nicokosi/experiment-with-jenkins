d7e7c3af8fe1afd64b2cc6dd9f36c60458c1cb90
Nicolas Kosinski <nicokosi@yahoo.com>
2020-08-16 15:13:40 +0200

    More cleaning

--- clean.sh
+++ clean.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Stop and remove Docker containers..."
 docker stop nicokosi-jenkins jenkins-docker
 docker rm nicokosi-jenkins jenkins-docker
+
+echo "Stop and remove Docker network..."
 docker network rm jenkins
+
+echo "Stop and remove Docker volumes..."
+docker volume rm jenkins-docker-certs jenkins-data
+
+echo "Done! ✅"
